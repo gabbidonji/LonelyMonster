c2a49c39a125a7c75be0774421f6729cd0233d76
EmilyMattlin <emilyjmattlin@gmail.com>
2019-10-22 10:30:37 -0400

    added camera movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -22,27 +22,39 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField]
     GameObject attackHitbox;
+
+    public enum RotationAxis
+    {
+        MOUSEX = 1
+    }
+
+    public RotationAxis axes = RotationAxis.MOUSEX;
+    public float HorizontalSpeed = 30.0f;
+    public float rotationX = 0;
+
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-        if (!found)
-        {
-            transform.rotation = Quaternion.Euler(0,playerWorldRelationAngle,0);
+         float moveHorizontal = Input.GetAxis("Horizontal");
+         float moveVertical = Input.GetAxis("Vertical");
+         if (!found)
+         {
+             Vector3 movement = new Vector3(moveVertical, 0.0f, -moveHorizontal);
+             GetComponent<Rigidbody>().velocity = transform.TransformDirection(movement) * speed;
+         } else {
+             GetComponent<Rigidbody>().velocity = new Vector3();
+             Vector3 movement = new Vector3(moveVertical, 0.0f, -moveHorizontal);
+         }
 
-            Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-            GetComponent<Rigidbody>().velocity = transform.TransformDirection(movement) * speed;
-        } else {
-            GetComponent<Rigidbody>().velocity = new Vector3();
-            Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-            transform.rotation = Quaternion.LookRotation(movement);
+        if (axes == RotationAxis.MOUSEX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * HorizontalSpeed, 0);
         }
+
         if(Input.GetKeyDown("space") && !attacking)
         {
             attackHitbox.SetActive(true);
