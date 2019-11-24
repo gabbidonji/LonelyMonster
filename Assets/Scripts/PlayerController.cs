1f9651af0008da640810ba481a4f079960e23faf
Jini Gabbidon <jini@mit.edu>
2019-11-23 22:12:02 -0500

    Fixed strange movement from one enemy.  Camera no longer rotates when dead.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -95,7 +95,7 @@ public class PlayerController : MonoBehaviour
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-        if (axes == RotationAxis.MOUSEX)
+        if ((axes == RotationAxis.MOUSEX) && (state != PlayerState.DEAD))
         {
             transform.Rotate(0, Input.GetAxis("Mouse X") * HorizontalSpeed, 0);
         }
